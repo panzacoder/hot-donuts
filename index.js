f88bfa7edf2a1e431d32510f87860aa52e3633b4
Jacob Hebert <jacobshebert@gmail.com>
2019-05-22 15:25:21 -0500

    Set up alexa ask-sdk and other dependencies, working through guts of the app.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 //index.js
 
+'use strict';
+
+const Alexa = require('ask-sdk-core');
+const https = require('request');
+const uuidv4 = require('uuid/v4');
+
+const HOTDONUT_API_KEY = process.env.HOTDONUT_API_KEY;
+const SKILL_NAME = 'Hot Donuts';
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const express = require('express')
@@ -7,74 +15,25 @@ const app = express()
 const AWS = require('aws-sdk');
 
 
-const REQUESTS_TABLE = process.env.REQUESTS_TABLE;
 
-const IS_OFFLINE = process.env.IS_OFFLINE;
-let dynamoDb;
-if (IS_OFFLINE === 'true') {
-  dynamoDb = new AWS.DynamoDB.DocumentClient({
-    region: 'localhost',
-    endpoint: 'http://localhost:8000'
+function locationGet (lat, long) {
+  return new Promise (function (resolve, reject) {
+    let locations = 'Local donut shops.';
+    let uuid = uuidv4();
+    uuid = uuid.replace('-','');
+    let url = "https://services.krispykreme.com/api/locationsearchresult/?callback=jQuery"+uuid+"&responseType=Full&search=%7B%22Where%22%3A%7B%22LocationTypes%22%3A%5B%22Store%22%2C%22Franchise%22%5D%2C%22OpeningDate%22%3A%7B%22ComparisonType%22%3A0%7D%2C%22HasGrubHub%22%3A0%2C%22HasUberEats%22%3A0%2C%22HasAdvancedOrdering%22%3A0%2C%22HasMobileOrdering%22%3A0%2C%22HasDriveThru%22%3A0%7D%2C%22Take%22%3A%7B%22Min%22%3A3%2C%22DistanceRadius%22%3A100%7D%2C%22PropertyFilters%22%3A%7B%22Attributes%22%3A%5B%22OpeningType%22%5D%7D%7D&lat="+lat+"&lng="+long
+    https (url, options, (err, res, body) => {
+      if (err) {
+        console.log ("Error retrieving donuts.");
+        reject (new Error ("Error retrieving donuts."));
+      }
+      console.log (body.url);
+      console.log (body);
+
+      
+    })
   })
-  console.log(dynamoDb);
-} else {
-  dynamoDb = new AWS.DynamoDB.DocumentClient();
-};
-
-app.use(bodyParser.json({ strict: false }));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-// Get Request endpoint
-app.get('/requests/:requestId', function (req, res) {
-  const params = {
-    TableName: REQUESTS_TABLE,
-    Key: {
-      requestId: req.params.requestId,
-    },
-  }
-
-  dynamoDb.get(params, (error, result) => {
-    if (error) {
-      console.log(error);
-      res.status(400).json({ error: 'Could not get request' });
-    }
-    if (result.Item) {
-      const {requestId, name} = result.Item;
-      res.json({ requestId, name });
-    } else {
-      res.status(404).json({ error: "Request not found" });
-    }
-  });
-})
-
-// Create Request endpoint
-app.post('/requests', function (req, res) {
-  const { requestId, name } = req.body;
-  if (typeof requestId !== 'string') {
-    res.status(400).json({ error: '"requestId" must be a string' });
-  } else if (typeof name !== 'string') {
-    res.status(400).json({ error: '"name" must be a string' });
-  }
-
-  const params = {
-    TableName: REQUESTS_TABLE,
-    Item: {
-      requestId: requestId,
-      name: name,
-    },
-  };
-
-  dynamoDb.put(params, (error) => {
-    if (error) {
-      console.log(error);
-      res.status(400).json({ error: 'Could not create request' });
-    }
-    res.json({ requestId, name });
-  });
-})
+}
 
 
 module.exports.handler = serverless(app);
